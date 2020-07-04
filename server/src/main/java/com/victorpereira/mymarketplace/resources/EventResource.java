c68621754aab0fb027d31f2d8703534e4cd44a68
Victor Pereira <victor.cordeiro2@gmail.com>
2020-07-04 18:54:56 -0300

    Event GET and POST

---
+++ server/src/main/java/com/victorpereira/mymarketplace/resources/EventResource.java
@@ -0,0 +1,41 @@
+package com.victorpereira.mymarketplace.resources;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.victorpereira.mymarketplace.models.Event;
+import com.victorpereira.mymarketplace.repositories.EventRepository;
+import com.victorpereira.mymarketplace.resources.exceptions.ObjectNotFoundException;
+
+@RestController
+@RequestMapping(value="/events")
+public class EventResource {
+	
+	@Autowired
+	private EventRepository eventRepo;
+	
+	@GetMapping
+	public List<Event> findAll(){
+		return eventRepo.findAll();
+	}
+	
+	@GetMapping(value = "/{id}")
+	public Event findById(@PathVariable Integer id) {
+		Optional<Event> user = eventRepo.findById(id);
+		return user.orElseThrow(
+				() -> new ObjectNotFoundException("Object not found! Id: " + id + ", Tipo: " + Event.class.getName()));
+	}
+	
+	@PostMapping
+	public Event insert(@RequestBody Event event) {
+		return eventRepo.save(event);
+	}
+}
