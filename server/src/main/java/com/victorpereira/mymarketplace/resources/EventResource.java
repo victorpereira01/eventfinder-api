28e44a933ce96a70b0cbfcdd2849332cc14b5bd0
Victor Pereira <victor.cordeiro2@gmail.com>
2020-07-04 19:11:05 -0300

    Event DELETE and PUT

--- server/src/main/java/com/victorpereira/mymarketplace/resources/EventResource.java
+++ server/src/main/java/com/victorpereira/mymarketplace/resources/EventResource.java
@@ -4,16 +4,20 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.victorpereira.mymarketplace.models.Event;
+import com.victorpereira.mymarketplace.models.User;
 import com.victorpereira.mymarketplace.repositories.EventRepository;
 import com.victorpereira.mymarketplace.resources.exceptions.ObjectNotFoundException;
+import com.victorpereira.mymarketplace.resources.utils.Utils;
 
 @RestController
 @RequestMapping(value="/events")
@@ -37,5 +41,19 @@ public class EventResource {
 	@PostMapping
 	public Event insert(@RequestBody Event event) {
 		return eventRepo.save(event);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public void delete(@PathVariable Integer id) {
+		Event user = findById(id);
+		eventRepo.delete(user);
+	}
+
+	@PutMapping(value = "/{id}")
+	public Event update(@RequestBody Event event, @PathVariable Integer id) {
+		Event obj = findById(id);
+		obj = Utils.validateEventFields(event, obj);
+		
+		return eventRepo.save(obj);
 	}
 }
--- server/src/main/java/com/victorpereira/mymarketplace/resources/utils/Utils.java
+++ server/src/main/java/com/victorpereira/mymarketplace/resources/utils/Utils.java
@@ -1,5 +1,6 @@
 package com.victorpereira.mymarketplace.resources.utils;
 
+import com.victorpereira.mymarketplace.models.Event;
 import com.victorpereira.mymarketplace.models.User;
 
 public class Utils {
@@ -16,6 +17,28 @@ public class Utils {
 		}
 		if (user.getPassword() != null) {
 			obj.setPassword(user.getPassword());
+		}
+		return obj;
+	}
+
+	public static Event validateEventFields(Event event, Event obj) {
+		if (event.getName() != null) {
+			obj.setName(event.getName());
+		}
+		if (event.getState() != null) {
+			obj.setState(event.getState());
+		}
+		if (event.getCity() != null) {
+			obj.setCity(event.getCity());
+		}
+		if (event.getPlace() != null) {
+			obj.setPlace(event.getPlace());
+		}
+		if (event.getInitialDate() != null) {
+			obj.setInitialDate(event.getInitialDate());
+		}
+		if (event.getEndDate() != null) {
+			obj.setEndDate(event.getEndDate());
 		}
 		return obj;
 	}
